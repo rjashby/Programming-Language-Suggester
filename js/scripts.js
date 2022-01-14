4917b82d94c820bfea004b496e333593fd671c19
Ryan Ashby <rjapsu160@gmail.com>
2022-01-14 13:04:22 -0800

    write all branching statements and test: passed

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,6 @@ $(document).ready(function() {
     const name = $("input#name").val();
     const age = parseInt($("input#age").val());
     const song = $("input:radio:checked").val();
-    // const beverage = $("input#beverage").val();
     const beverage = document.getElementById("beverage").value;
     const cool = $("input#cool").val();
     const phone = parseInt($("input#phone").val());
@@ -14,6 +13,7 @@ $(document).ready(function() {
     $("#python").hide();
     $("#swift").hide();
     $("#csharp").hide();
+    $("#toocool").hide();
     console.log(name);
     console.log(age);
     console.log(song);
@@ -23,8 +23,44 @@ $(document).ready(function() {
     console.log(santa);
     console.log(color);
 
-    // if (a=b) {
-
-    // }
+    if (cool >= 90) {
+      $("#toocool").show();
+    } else {
+      if (song === "shoot" && beverage === "beer") {
+        $("#javascript").show();
+      } else if (song === "shoot" && beverage === "wine") {
+        $("#python").show();
+      } else if (song === "shoot" && beverage === "booze") {
+        $("#swift").show();
+      } else if (song === "shoot" && beverage === "seltzer") {
+        $("#csharp").show();
+      } else if (song === "back" && beverage === "beer") {
+        $("#csharp").show();
+      } else if (song === "back" && beverage === "wine") {
+        $("#swift").show();
+      } else if (song === "back" && beverage === "booze") {
+        $("#python").show();
+      } else if (song === "back" && beverage === "seltzer") {
+        $("#javascript").show();
+      } else if (song === "thunder" && beverage === "beer") {
+        $("#javascript").show();
+      } else if (song === "thunder" && beverage === "wine") {
+        $("#python").show();
+      } else if (song === "thunder" && beverage === "booze") {
+        $("#swift").show();
+      } else if (song === "thunder" && beverage === "seltzer") {
+        $("#csharp").show();
+      } else if (song === "highway" && beverage === "beer") {
+        $("#csharp").show();
+      } else if (song === "highway" && beverage === "wine") {
+        $("#swift").show();
+      } else if (song === "highway" && beverage === "booze") {
+        $("#python").show();
+      } else if (song === "highway" && beverage === "seltzer") {
+        $("#javascript").show();
+      }
+    }
+  window.scrollTo(0,document.body.scrollHeight);
   });
 });
+

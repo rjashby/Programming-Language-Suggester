bfd06793433aa712c9b68dc4245ecfe71e144209
Ryan Ashby <rjapsu160@gmail.com>
2022-01-14 12:30:14 -0800

    add range scale input and corresponding variable

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ $(document).ready(function() {
     const name = $("input#name").val();
     const age = parseInt($("input#age").val());
     const song = $("input:radio:checked").val();
-    const beverage = $("input#beverage").val();
+    // const beverage = $("input#beverage").val();
+    const beverage = document.getElementById("beverage").value;
+    const cool = $("input#cool").val();
     const phone = parseInt($("input#phone").val());
     const santa = $("input#santa").val();
     const color = $("input#color").val();
@@ -16,12 +18,13 @@ $(document).ready(function() {
     console.log(age);
     console.log(song);
     console.log(beverage);
+    console.log(cool);
     console.log(phone);
     console.log(santa);
     console.log(color);
 
-    if (a=b) {
+    // if (a=b) {
 
-    }
+    // }
   });
 });

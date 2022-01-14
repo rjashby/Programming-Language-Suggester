9908f4cb1b0483f8c978f51dd44158231b04f681
Ryan Ashby <rjapsu160@gmail.com>
2022-01-14 10:46:10 -0800

    WIP console.log all input variables for testing: passed

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-  $("form").submit(function(event) {
-    event.preventDefault;
+  $("form#survey").submit(function(event) {
+    event.preventDefault();
     const name = $("input#name").val();
     const age = parseInt($("input#age").val());
     const song = $("input:radio:checked").val();
     const beverage = $("input#beverage").val();
-    const phone = parseInt($("input#").val());
-    const santa = $("input#").val();
-    const color = $("input#").val();
+    const phone = parseInt($("input#phone").val());
+    const santa = $("input#santa").val();
+    const color = $("input#color").val();
+    console.log(name);
+    console.log(age);
+    console.log(song);
+    console.log(beverage);
+    console.log(phone);
+    console.log(santa);
+    console.log(color);
   });
 });

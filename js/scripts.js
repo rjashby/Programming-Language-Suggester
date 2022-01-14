8e2de89c0365635a4bff7d5bbe107aea35286792
Ryan Ashby <rjapsu160@gmail.com>
2022-01-14 09:44:04 -0800

    add doc.ready and submit funct with vars for input vals

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault;
+    const name = $("input#name").val();
+    const age = parseInt($("input#age").val());
+    const insert = $("input:radio:checked").val();
+    const insert = $("input#").val();
+    const insert = parseInt($("input#").val());
+    const insert = $("input#").val();
+    const insert = $("input#").val();
+  });
+});
